4a49d862f24ebab83400ce285b0c8f9ed374253e
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-19 08:00:59 -0700

    Added module rules, commented out miniCSSExtractplugin for now

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,24 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        // {
+        //   test: /\.css$/i,
+        //   use: ["style-loader", 'css-loader'],
+        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
