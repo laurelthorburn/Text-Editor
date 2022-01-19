4fdbb110bffe8bf874bc9b76905f6b731d732e57
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-19 07:48:00 -0700

    Entered scripts

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
