2551e726add82fc44364b5f5239b109c604d8243
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-19 07:48:35 -0700

    Added comments to server

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Requires HTML route
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT} 🍾`));
