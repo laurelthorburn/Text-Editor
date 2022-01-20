7199e25b14fae521db40c99047c40aa27ca7b24a
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-20 08:01:50 -0700

    Added comments to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,17 +1,24 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+// We are creating a new database named 'contact' which will be using version 1 of the database.
+
   openDB('jate', 1, {
+        // Add our database schema if it has not already been initialized.
+
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+            // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
-
+  
+// Export a function we will use to POST to the database.
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
 
