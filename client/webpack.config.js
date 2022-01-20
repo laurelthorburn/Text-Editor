6a52a939935170cee6b949b1fc6ec188588f01ed
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-20 07:51:42 -0700

    Updating webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // Require the GenerateSW class of the WorkBoxPlugin 
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
@@ -26,6 +25,33 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
+           
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+            // Creates a manifest.json file.
+            new WebpackPwaManifest({
+              fingerprints: false,
+              inject: true,
+              name: 'JATE Logo',
+              short_name: 'HATE',
+              description: 'Laurel strongly dislikes JATE!',
+              background_color: '#225ca3',
+              theme_color: '#225ca3',
+              start_url: '/',
+              publicPath: '/',
+              icons: [
+                {
+                  src: path.resolve('src/images/logo.png'),
+                  sizes: [96, 128, 192, 256, 384, 512],
+                  destination: path.join('assets', 'icons'),
+                },
+              ],
+            }),
+// do i need this line??
       new WorkboxPlugin.GenerateSW()
     ],
 
@@ -36,12 +62,9 @@ module.exports = () => {
           use: ["style-loader", 'css-loader'],
         },
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
+                    // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
