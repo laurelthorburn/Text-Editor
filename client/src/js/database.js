80b8c2cac3038a7cdfec49afc793704057d5d496
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-20 12:42:07 -0700

    Cereated database get and put routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -4,25 +4,73 @@ const initdb = async () =>
 // We are creating a new database named 'contact' which will be using version 1 of the database.
 
   openDB('jate', 1, {
-        // Add our database schema if it has not already been initialized.
-
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
-            // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
-
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+    // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+    db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created 🖖');
     },
   });
-  
+
 // Export a function we will use to POST to the database.
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+
+// Export a function we will use to POST to the database.
+export const putDb = async (content)  => {
+  console.log('Update the database, what a giver');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('contact', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('contact', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('contact');
+
+  // Use the .add() method on the store and pass in the content.
+  //IDK IF I DID THIS PART RIGHT!!!!!!!!!!!!
+  const request = store.add({ content: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+
+  //can i catch the error here?
+  // if (error) {
+  //   console.error('putDb not implemented');
+  // }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// export const getDb = async () => console.error('getDb not implemented');
 
+// Export a function we will use to GET to the database.
+export const getDb = async () => {
+  console.log('GET from the database, so greedy');
+
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('contact', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('contact', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('contact');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+// Start the database
 initdb();
